94ad4b7a87b71775d344b9ab07e4dd84c48cff12
Heitor França <89424453+Heitorfrc@users.noreply.github.com>
2022-07-26 11:35:28 -0300

    Escolhendo a melhor categoria de habilitação

---
+++ Heitor_habilitacao.py
@@ -0,0 +1,31 @@
+'''
+Desenvolva um código que utilize as seguintes características de um veículo:
+- Quantidade de rodas;
+- Peso bruto em quilogramas;
+- Quantidade de pessoas no veículo.
+
+Com essas informações, o programa mostrará qual é a melhor categoria de habilitação para o veículo informado a partir das condições:
+A: Veículos com duas ou três rodas;  A
+B: Veículos com quatro rodas, que acomodam até oito pessoas e seu peso é de até 3500 kg;  B
+C: Veículos com quatro rodas ou mais e com peso entre 3500 e 6000 kg;  C
+D: Veículos com quatro rodas ou mais e que acomodam mais de oito pessoas;  D
+E: Veículos com quatro rodas ou mais e com mais de 6000 kg.  E
+'''
+#Nesse exercício algumas categorias se sobrepoem, vamos usar o bom senso e escolher a melhor categoria pela ordem que foi dada
+
+rodas = int(input("Digite o número de rodas do veículo: "))
+peso = float(input("Digite o peso bruto em Kg do veículo: "))
+pessoas = int(input("Digite o número de pessoas no veículo: "))
+
+if rodas > 4 and pessoas < 8 and peso < 6000 :
+    print("A categoria de habilitaçâo do veículo é B")
+elif rodas > 4 and peso < 6000 :
+    print("A categoria de habilitaçâo do veículo é C")
+elif rodas > 4 and pessoas > 8 :
+    print("A categoria de habilitaçâo do veículo é D")
+elif rodas > 4 and peso > 6000 :
+    print("A categoria de habilitaçâo do veículo é E")
+elif rodas < 4 and rodas > 1 :
+    print("A categoria de habilitaçâo do veículo é A")
+else :
+    print("Escolha um veículo que possa ser habilitado")
