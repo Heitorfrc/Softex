516c05d9d66753bcc0621589dc8a82efa71547c1
Heitor França <89424453+Heitorfrc@users.noreply.github.com>
2022-10-10 17:49:56 -0300

    Add files via upload
    Criando pacotes e classes em Java

---
+++ Packages-Classes.java
@@ -0,0 +1,35 @@
+/*
+Crie dois pacotes baseados nas atribuições de uma empresa e inclua classes neles. Lembre-se de trabalhar com os padrões Java.
+*/
+
+package br.com.papelaria.inventorio;
+
+import java.io.*;
+import java.util.*;
+
+public class Caneta {
+    public static void main(Int[] args) {
+        Scanner cane = new Scanner(System.in);
+        System.out.println("Qual a quantidade de caneta?");
+        qntCane = cane.nextLine();
+        System.out.println("Your Name IS : " + qntCane);
+    }
+}
+
+public class Lapis {
+    public static void main(Int[] args) {
+        Scanner lap = new Scanner(System.in);
+        System.out.println("Qual a quantidade de caneta?");
+        qntLap = cane.nextLine();
+        System.out.println("Your Name IS : " + qntLap); 
+    }
+}
+
+package br.com.papelaria.conta;
+
+public class Cliente {
+    public static void main(Int[] args) {
+        String nomeCliente;
+        Int codCliente;
+    }
+}
