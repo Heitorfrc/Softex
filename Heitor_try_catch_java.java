8f9f6830e0999299215b4b8d4857b70fe08594e8
Heitor França <89424453+Heitorfrc@users.noreply.github.com>
2022-10-24 17:48:53 -0300

    Update Heitor_try_catch_java.java

--- Heitor_try_catch_java.java
+++ Heitor_try_catch_java.java
@@ -1,12 +1,17 @@
 /*
-Crie uma situação em que ocorra uma exceção dentro de um código. Utilize try/catch para realizar a captura e tratamento dessa exceção.
+Crie uma situação em que ocorra uma exceção dentro de um código. Utilize try/catch para realizar a captura e tratamento dessa exceção. Em JAVA
 */
 
 public class Main {
     public static void main(String[ ] args) {
+      int[] dados = {0, 1, 2, 3, 4};
       try {
-        int[] dados = {0, 1, 2, 3, 4};
         System.out.println(dados[5]);
+      } catch (Exception e) {
+        System.out.println("Ocorreu o erro: " + e.getMessage());
+      }
+      try {
+        System.out.println(dados[2]/dados[0]);
       } catch (Exception e) {
         System.out.println("Ocorreu o erro: " + e.getMessage());
       }
